f08a63554dd6116815868374934d33071f2fe636
sergey <sersh200606@gmail.com>
2025-12-13 16:25:08 +0300

    readme

--- src/main/java/com/serjn/online/controllers/ClientController.java
+++ src/main/java/com/serjn/online/controllers/ClientController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/client")
+@RequestMapping("/api/v1/me")
 @RequiredArgsConstructor
 @Validated
 public class ClientController {
